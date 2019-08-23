de28d73c98816c7475988ecebc94940de1eb939a
Hemaraj <hemaraj.gajaraj@freshworks.com>
2019-08-23 10:49:03 +0530

    username and email config pending

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,29 @@
 class SessionsController < ApplicationController
-
+  
+  include EmailHelper
 	def new
   end
 
    def create
+
+    user = params[:session][:name]
+
+    if isEmail(user)
+
+    	user = User.find_by_email(params[:session][:email].downcase)
+
+    else
+    	
     user = User.find_by_name(params[:session][:name].downcase)
+   
+    end
 
     if user && user.authenticate(params[:session][:password])
       
       sign_in user
       redirect_to user
+
+
 
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,9 @@ class UsersController < ApplicationController
    def show
     @user = User.find(params[:id])
   end
+
+   def edit
+    @user = User.find(params[:id])
+  end
+  
 end
